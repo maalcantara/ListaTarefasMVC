5ae8dece5d4fd8a304dad275dbd6d2394e0aef9d
mariana <marigigialcantara40@gmail.com>
2024-03-07 20:41:12 -0300

    add model error

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ListaTarefasMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
